c0fc62a45b4e9397569a814a1b84a4ad4e3d90fd
Francineide Oliveira <frneide@alu.ufc.br>
2025-08-19 18:51:38 -0300

    funções - adicionar e exibir lista de adicionados
    Capturar o valor do campo de entrada;
    Validar a entrada; Limpar o campo de entrada.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let lista_de_amigos = [];
+
+function add_amigos() {
+    let amigos = document.querySelector('input').value;
+
+    if (amigos == '') {
+        alert('Por favor, digite um nome.');
+
+    } else {
+        lista_de_amigos.push(amigos);
+        document.querySelector('input').value = '';
+        exibir_amigos();
+    }
+}
+
+function exibir_amigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    if (lista_de_amigos.length == 0) {
+        lista.innerHTML = '<p class="aviso">Nenhum amigo adicionado.</p>';
+    } else {
+        lista_de_amigos.forEach((amigo, index) => {
+            lista.innerHTML += `<li>${index + 1} - ${amigo}</li>`;
+        });
+    }    
+}
